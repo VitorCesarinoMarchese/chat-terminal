43660982d5a95f8e208a498bd6bde2123037a2cb
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2025-08-05 12:50:01 -0300

    feat:

--- tui/cmd/main.go
+++ tui/cmd/main.go
@@ -4,6 +4,7 @@ import "github.com/VitorCesarinoMarchese/chat-terminal/internal/app"
 
 func main() {
 	application := app.NewApp()
+	app.Db()
 	if err := application.Run(); err != nil {
 		panic(err)
 	}
