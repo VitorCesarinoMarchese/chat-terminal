5ccba99be45749103b93d75bb483de1e93d60155
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2025-08-09 13:17:37 -0300

    Test: input behavior

--- tui/internal/screens/test.go
+++ tui/internal/screens/test.go
@@ -14,11 +14,38 @@ func Test(app *tview.Application) tview.Primitive {
 		"[blue]User2: [white]Looks like it's working!",
 		"[green]User1: [white]Sweet! Let’s add some more messages...",
 		"[blue]User2: [white]Sure thing!",
+		"[green]User1: [white]Hey, how are you?",
+		"[blue]User2: [white]I'm doing fine, thanks! You?",
+		"[green]User1: [white]Pretty good. Just testing this widget.",
+		"[blue]User2: [white]Looks like it's working!",
+		"[green]User1: [white]Sweet! Let’s add some more messages...",
+		"[blue]User2: [white]Sure thing!",
+		"[green]User1: [white]Hey, how are you?",
+		"[blue]User2: [white]I'm doing fine, thanks! You?",
+		"[green]User1: [white]Pretty good. Just testing this widget.",
+		"[blue]User2: [white]Looks like it's working!",
+		"[green]User1: [white]Sweet! Let’s add some more messages...",
+		"[blue]User2: [white]Sure thing!",
+		"[green]User1: [white]Hey, how are you?",
+		"[blue]User2: [white]I'm doing fine, thanks! You?",
+		"[green]User1: [white]Pretty good. Just testing this widget.",
+		"[blue]User2: [white]Looks like it's working!",
+		"[green]User1: [white]Sweet! Let’s add some more messages...",
+		"[blue]User2: [white]Sure thing!",
 	}
 
 	testSubject := widgets.Conversations(messages, app)
+	input := tview.NewInputField().
+		SetLabel("message: ").
+		SetFieldBackgroundColor(tcell.ColorGreen).
+		SetLabelColor(tcell.ColorOrchid)
 
-	frame := tview.NewFrame(testSubject).
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexColumnCSS).
+		AddItem(testSubject, 0, 19, false).
+		AddItem(input, 0, 1, true)
+
+	frame := tview.NewFrame(flex).
 		SetBorders(2, 2, 2, 2, 4, 4).
 		AddText("TUI-Chat", true, tview.AlignCenter, tcell.ColorGreen)
 
