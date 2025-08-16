b8cb2a76b7edea71f902e741f0eb6b272223213d
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2025-08-16 11:03:02 -0300

    feat: friendship reject and accept controllers

--- backend/src/controllers/friend.ts
+++ backend/src/controllers/friend.ts
@@ -5,8 +5,8 @@ import db from "../config/db"
 export const sentFriendInvitationController = async (req: Request, res: Response) => {
   try {
     const headerToken = req.headers['authorization']
-    const { senderId, receiverUsername } = req.body
-    if (!senderId || !receiverUsername) {
+    const { senderUsername, receiverUsername } = req.body
+    if (!senderUsername || !receiverUsername) {
       res.status(400).json({ error: "Missing data" })
       return
     }
@@ -16,7 +16,19 @@ export const sentFriendInvitationController = async (req: Request, res: Response
       return;
     }
 
-    const isAccessTokenValid = verifyAccessToken(headerToken, receiverUsername)
+    const sender = await db.user.findUnique({
+      where: {
+        username: senderUsername
+      },
+      select: { id: true }
+    })
+    if (!sender) {
+      res.status(400).json({ error: "Your user is incorrect" })
+      return
+    }
+    const senderId = sender.id
+
+    const isAccessTokenValid = verifyAccessToken(headerToken, senderId.toString())
     if (!isAccessTokenValid) {
       res.status(403).json({ error: "Invalid or expired Token" });
       return
@@ -56,10 +68,128 @@ export const sentFriendInvitationController = async (req: Request, res: Response
         receiverId: receiver.id,
         requesterId: senderId,
       },
-      select: { id: true, requesterId: true, receiverId: true }
+      select: { id: true, status: true }
     })
 
     res.status(201).json({ message: "Friendship request sent successufuly", request: friendRequest })
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+export const acceptFriendRequestController = async (req: Request, res: Response) => {
+  try {
+    const headerToken = req.headers['authorization']
+    const { requestId, username } = req.body
+    if (!requestId || !username) {
+      res.status(400).json({ error: "Missing data" })
+      return
+    }
+
+    if (!headerToken) {
+      res.status(403).json({ error: "Access denied" });
+      return;
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        username: username
+      },
+      select: { id: true }
+    })
+    if (!user) {
+      res.status(400).json({ error: "Your user is incorrect" })
+      return
+    }
+    const userId = user.id
+
+    const isAccessTokenValid = verifyAccessToken(headerToken, userId.toString())
+    if (!isAccessTokenValid) {
+      res.status(403).json({ error: "Invalid or expired Token" });
+      return
+    }
+
+    const isRequested = await db.friendship.findFirst({
+      where: {
+        id: requestId
+      },
+      select: { id: true, status: true }
+    })
+    if (!isRequested) {
+      res.status(400).json({ error: "Friendship request does not exists" })
+      return
+    }
+
+    const friendRequest = await db.friendship.update({
+      where: {
+        id: requestId
+      },
+      data: {
+        status: 'ACCEPTED'
+      },
+      select: { id: true, status: true }
+    })
+
+    res.status(201).json({ message: "Friendship request accepted successufuly", request: friendRequest })
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+export const regectFriendRequestController = async (req: Request, res: Response) => {
+  try {
+    const headerToken = req.headers['authorization']
+    const { requestId, username } = req.body
+    if (!requestId || !username) {
+      res.status(400).json({ error: "Missing data" })
+      return
+    }
+
+    if (!headerToken) {
+      res.status(403).json({ error: "Access denied" });
+      return;
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        username: username
+      },
+      select: { id: true }
+    })
+    if (!user) {
+      res.status(400).json({ error: "Your user is incorrect" })
+      return
+    }
+    const userId = user.id
+
+    const isAccessTokenValid = verifyAccessToken(headerToken, userId.toString())
+    if (!isAccessTokenValid) {
+      res.status(403).json({ error: "Invalid or expired Token" });
+      return
+    }
+
+    const isRequested = await db.friendship.findFirst({
+      where: {
+        id: requestId
+      },
+      select: { id: true, status: true }
+    })
+    if (!isRequested) {
+      res.status(400).json({ error: "Friendship request does not exists" })
+      return
+    }
+
+    const friendRequest = await db.friendship.update({
+      where: {
+        id: requestId
+      },
+      data: {
+        status: 'REJECTED'
+      },
+      select: { id: true, status: true }
+    })
+
+    res.status(201).json({ message: "Friendship request rejected successufuly", request: friendRequest })
   } catch (e) {
     res.status(500).json({ error: "Internal server error" })
   }
