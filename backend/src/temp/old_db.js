22653b90abff544a06e92e22d563bf0f0dfb7044
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2026-04-15 20:37:44 -0300

    fix: removed unused code

--- backend/src/temp/old_db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-var client_1 = require("@prisma/client");
-var db = new client_1.PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-});
-exports.db = db;
--- backend/src/temp/old_db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const db = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-});
-
-export { db };
-
